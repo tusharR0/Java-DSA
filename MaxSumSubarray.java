4bcfecc2d45e2a32ea9f12176869a482f124aec2
tusharR0 <tusharraut704@gmail.com>
2025-12-23 11:08:42 +0530

    added new q.

---
+++ MaxSumSubarray.java
@@ -0,0 +1,22 @@
+public class MaxSumSubarray {
+    public static void main(String[] args) {
+        int[] arr = {2, 1, 5, 1, 3, 2};
+        int k = 3;
+
+        int windowSum = 0;
+        int maxSum = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            windowSum += arr[i];
+
+            if (i >= k - 1) {
+                maxSum = Math.max(maxSum, windowSum);
+                windowSum -= arr[i - (k - 1)];
+            }
+        }
+
+        System.out.println("Maximum Sum = " + maxSum);
+    }
+}
+
+
--- MaxVowels.java
+++ MaxVowels.java
@@ -1,27 +1,6 @@
-// public class MaxSumSubarray {
-//     public static void main(String[] args) {
-//         int[] arr = {2, 1, 5, 1, 3, 2};
-//         int k = 3;
-
-//         int windowSum = 0;
-//         int maxSum = 0;
-
-//         for (int i = 0; i < arr.length; i++) {
-//             windowSum += arr[i];
-
-//             if (i >= k - 1) {
-//                 maxSum = Math.max(maxSum, windowSum);
-//                 windowSum -= arr[i - (k - 1)];
-//             }
-//         }
-
-//         System.out.println("Maximum Sum = " + maxSum);
-//     }
-// }
-// 🔸 Maximum Vowels in Substring of Length k
 public class MaxVowels {
     public static void main(String[] args) {
-        String s = "abciiidef";
+        String s = "us";
         int k = 3;
 
         int count = 0, max = 0;
@@ -40,7 +19,6 @@ public class MaxVowels {
     }
 
     static boolean isVowel(char c) {
-        return "aeiou".indexOf(c) != -1;
+        return "io".indexOf(c) != -1;
     }
 }
-
